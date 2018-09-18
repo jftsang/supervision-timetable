c46435455b864fda91971ca5eb8c530deb141d89
jftsang <jmft2@cam.ac.uk>
2018-09-18 15:47:20 +0100

    Some style changes, especially for breaks

--- index.php
+++ index.php
@@ -6,15 +6,20 @@ $file = fopen($filename,'r');
 $supervisions = array();
 while($line = fgets($file)) {
     /* Ignore comments */
-    if (substr($line,1,1) == '#') 
+    if (substr($line,0,1) == '#' || substr($line,0,1) == '%') 
         continue;
 
     list($year,$month,$day,$hour,$min,$course,$location,$people) = sscanf($line,
         "%4d-%2d-%2d %2d:%2d %[^;]; %[^;]; %[^\n]\n");
+    $currtime = $time;
     $time = strtotime("$year-$month-$day $hour:$min");
+    if ($time === false) 
+        $time = $currtime;
+
     $supervisions[] = array('time' => $time, 'course' => $course, 
         'location'=>$location, 'people' => $people );
 }
+// var_dump($supervisions);
 
 /* Sort supervisions by date and time */
 function cmp($a,$b) {
@@ -73,13 +78,23 @@ function weeksbetween($time1,$time2) {
 
 <p style="text-align: center;">Today is <?=date('D d M Y')?>.</p>
 
-<p>Please feel free to swap supervision slots with each other. <a
-href="mailto:jmft2@cam.ac.uk">Email me</a> to let me know, and <em>please copy
-in everyone else who's involved in that transaction</em>: this helps me know that
-you all assent to the swap.</p>
+<p><strong>Rearranging</strong> Please feel free to swap supervision slots with
+each other. <a href="mailto:jmft2@cam.ac.uk">Email me</a> to let me know, and
+<em>please copy in everyone else who's involved in that transaction</em>: this
+helps me know that you all assent to the swap.</p>
 
-<p>I'm not meant to meddle with supervision <em>pairings</em> &mdash; talk to
-your DoS about those.</p>
+<p>I'm not meant to meddle with supervision pairings &mdash; please talk to your
+DoS about those.</p>
+
+<p><strong>Deadlines</strong> Please hand your work in to my CMS pigeonhole,
+DAMTP 'T', by 4pm on these dates:
+<ul>
+  <li>Queens', sheet 1: Tuesday 2 October</li>
+  <li>Queens', sheet 2: Tuesday 9 October</li>
+  <li>Murray Edwards, sheet 1: Friday 5 October</li>
+  <li>Murray Edwards, sheet 2: Friday 12 October</li>
+</ul>
+</p>
 
 <!--
 <p><strong>Mathematical Biology</strong> Please hand in your work by Sunday 2pm
@@ -109,6 +124,13 @@ you.</p>
 $mintime = isset($_REQUEST['mintime'])?$_REQUEST['mintime']:(time()-3600*24);
 $maxtime = isset($_REQUEST['maxtime'])?$_REQUEST['maxtime']:INF;
 foreach($supervisions as &$supervision) {
+    /* Put blank lines for breakers */
+    if ($supervision['course'] === NULL)
+    {
+    //    printf("<tr><td colspan=\"6\" style=\"text-align:center; padding-top: 10px; padding-bottom: 10px\"></td></tr>");
+        continue;
+    }
+
     /* Ignore if outside the range of interest. */
     if ($supervision['time'] < $mintime || $supervision['time'] > $maxtime) 
         continue;
@@ -149,12 +171,28 @@ foreach($supervisions as &$supervision) {
 <tr>
 <td><a href="mailto:jmft2@cam.ac.uk">Email me</a></td>
 <td><a href="timetable.txt">Raw timetable</a></td>
-<td><a href="https://github.com/jftsang/supervision-timetable/">Plagiarise this page</a><br/>
-(see also <a href="https://www.youtube.com/watch?v=IL4vWJbwmqM">the song!</a>)</td>
+<td style="text-align: center;"><a href="https://github.com/jftsang/supervision-timetable/">Plagiarise this page</a><br/>
+(<a href="https://www.youtube.com/watch?v=IL4vWJbwmqM">the song</a>)</td>
 <td><a href="..">Home</a></td>
 </tr>
 </tbody></table>
 </div>
+
+
+<div style="margin-left: auto; margin-right: auto;">
+    <p style="text-align: center;">
+    <a href="http://validator.w3.org/check?uri=referer"><img
+      src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+&nbsp;
+    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+        <img style="border:0;width:88px;height:31px"
+            src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+            alt="Valid CSS!" />
+    </a>
+    </p>
+</div>
+            
+
 </div>
 </body>
 </html>
